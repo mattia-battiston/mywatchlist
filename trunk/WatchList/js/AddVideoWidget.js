54e29251af0a4216c352bccfbd7998e9bce50c67
mattia.battiston@gmail.com <mattia.battiston@gmail.com@a4d197d1-d1a3-f63c-8c95-cb920e4aaf9f>
2011-04-10 15:54:02 +0000

    Give a title to the video and Make url selected when opening popup

--- trunk/WatchList/js/AddVideoWidget.js
+++ trunk/WatchList/js/AddVideoWidget.js
@@ -6,12 +6,21 @@ AddVideoWidget.prototype.init = function() {
 
 AddVideoWidget.prototype.loadUrlInBox = function() {
 	chrome.tabs.getSelected(null, function(tab) {
+		document.getElementById("addVideoTitle").value = tab.title;
 		document.getElementById("addVideoUrl").value = tab.url;
+		
+		selectText();
 	});
 }
 
 AddVideoWidget.prototype.addVideo = function(){
+	var videoTitle = document.getElementById("addVideoTitle").value;
 	var videoUrl = document.getElementById("addVideoUrl").value;
-	watchListDao.insertVideo("my new video", videoUrl);
+	watchListDao.insertVideo(videoTitle, videoUrl);
 	refresh();
+}
+
+function selectText(){
+	document.getElementById("addVideoTitle").focus();
+    document.getElementById("addVideoTitle").select();
 }
